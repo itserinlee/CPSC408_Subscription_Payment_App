29d38f29ece0b7386dafa408e2b8889fd078e300
Tarek <elhaj102@mail.chapman.edu>
2021-12-07 00:28:46 -0800

    able to generate a rand value for each field

--- data/data_helper.py
+++ data/data_helper.py
@@ -1,9 +1,23 @@
+import names
+import datetime
+import random
+import randomname
 from os import stat
-
-
+from password_generator import PasswordGenerator
+from random_address import real_random_address
 from queries.read.queries import QUERIES as RE_QUERIES
+from categories import CATEGORIES
 
 class data_helper():
+        SEED=8
+        pwo = PasswordGenerator()
+        pwo.minlen = 4
+        pwo.maxlen = 6
+        pwo.minschars = 0
+        random.seed(SEED)
+        lst_categories = CATEGORIES
+
+        # DB tables IDs accessor methods
         @staticmethod
         def get_mag_ids(db):
                 return db.get_record_ids(RE_QUERIES["MAG_GET_IDS"])
@@ -19,3 +33,45 @@ class data_helper():
         @staticmethod
         def get_pay_ids(db):
                 return db.get_record_ids(RE_QUERIES["PAY_GET_IDS"])
+
+        # helper methods for generating values for the different table attributes
+        @staticmethod
+        def gen_rand_pw(pwo=pwo):
+                return pwo.generate()
+
+        @staticmethod
+        def gen_rand_status():
+                return random.randrange(0, 1)
+
+        @staticmethod
+        def gen_rand_num(x=20, y=300):
+                return random.randrange(x, y)
+        
+        @staticmethod
+        def gen_rand_date(s_year = 2000, s_m = 1, s_d=1, e_year=2021, e_m=12, e_d=31):
+                # src: https://www.kite.com/python/answers/how-to-generate-a-random-date-between-two-dates-in-python
+                start_date = datetime.date(s_year, s_m, s_d)
+                end_date = datetime.date(e_year, e_m, e_d)
+
+                time_between_dates = end_date - start_date
+                days_between_dates = time_between_dates.days
+                random_number_of_days = random.randrange(days_between_dates)
+                random_date = start_date + datetime.timedelta(days=random_number_of_days)
+                return random_date
+        
+        @staticmethod
+        def gen_rand_addr():
+                return real_random_address()
+        # rand_add = real_random_address()
+        # zip_code = rand_add['postalCode']
+        # state = rand_add['state']
+        # city = rand_add['city']
+        # street_addr = rand_add['address1']
+        # contact_type = randint(0,1)
+
+        @staticmethod
+        def gen_rand_nandcat(categories=lst_categories):
+                get_rand_cat = lambda : categories[random.randrange(0, len(categories) - 1)]
+                rand_cat = get_rand_cat()
+                rand_name = randomname.get_name(noun=(rand_cat))
+                return { "name": rand_name, "category": rand_cat } 
--- data/gen_data.py
+++ data/gen_data.py
@@ -1,13 +1,15 @@
 import sys
 import os
+import names
 
+from password_generator import PasswordGenerator
 from dotenv import load_dotenv
 load_dotenv()
 
 PROJ_DIR_ABS_PATH = os.getenv("PROJ_DIR_ABS_PATH")
 
 try:
-        from controllers.db_helper import db_helper
+        from models.db_model import DB_Model
 except ModuleNotFoundError as err:
         print("Fixing sys path..")
         sys.path.insert(0, PROJ_DIR_ABS_PATH)
@@ -18,11 +20,12 @@ from data.data_helper import data_helper as helper
 
 columns = [
         "cust_id","cust_first_name","cust_last_name","cust_username","cust_password",
-        "cust_record_create_date","mag_id","mag_name","mag_cost","mag_record_status","mag_record_create_date",
-        "prof_cust_cont_id","prof_phone","prof_zip","prof_state","prof_city","prof_street_address","prof_contact_type",
-        "prof_record_update_stamp","prof_record_status","prof_start_date","prof_end_date","sub_id","sub_num_mags_mailed",
-        "sub_payment_completed","sub_start_date","sub_end_date","pay_id","pay_amount","pay_type","pay_date","pay_card_num",
-        "pay_card_code","pay_record_create_date","category"
+        "cust_record_create_date","mag_id","mag_name","mag_cost","mag_record_status",
+        "mag_record_create_date","prof_cust_cont_id","prof_phone","prof_zip","prof_state",
+        "prof_city","prof_street_address","prof_contact_type","prof_record_update_stamp",
+        "prof_record_status","prof_start_date","prof_end_date","sub_id","sub_num_mags_mailed",
+        "sub_payment_completed","sub_start_date","sub_end_date","pay_id","pay_amount",
+        "pay_type","pay_date","pay_card_num","pay_card_code","pay_record_create_date","category"
         ]
 
 def gen_rand_data():
@@ -45,17 +48,12 @@ def gen_rand_data():
 
         db.destructor()
 
+pwo = PasswordGenerator()
+pwo.minlen = 4
+pwo.maxlen = 6
+pwo.minschars = 0
 
-gen_rand_data()
+for i in range(30):
+        print(helper.gen_rand_nandcat())
 
-# helper methods
-# def get_mag_ids(db):
-#         return db.get_record_ids(RE_QUERIES["MAG_GET_IDS"])
-# def get_cust_ids(db):
-#         return db.get_record_ids(RE_QUERIES["CUST_GET_IDS"])
-# def get_pro_ids(db):
-#         return db.get_record_ids(RE_QUERIES["PRO_GET_IDS"])
-# def get_sub_ids(db):
-#         return db.get_record_ids(RE_QUERIES["SUB_GET_IDS"])
-# def get_pay_ids(db):
-#         return db.get_record_ids(RE_QUERIES["PAY_GET_IDS"])
+# gen_rand_data()
