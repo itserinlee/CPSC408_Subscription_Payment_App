5a6f218a07331abc8f5f6255f56e53093c8ea4eb
Erin Lee <elee2@chapman.edu>
2021-12-18 02:31:06 -0800

    updating again

--- queries/update/transactions.py
+++ queries/update/transactions.py
@@ -4,7 +4,7 @@ import mysql.connector
 connection = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "Nosplits35!",
+    password = "",
     database = "final_project"
 )
 
