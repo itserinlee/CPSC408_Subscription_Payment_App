cd348f457be1cac64c0352e445ede34b0b72f749
Erin Lee <elee2@chapman.edu>
2021-12-17 22:55:41 -0800

    adding transaction trigger code

--- queries/update/modifyTables.py
+++ queries/update/modifyTables.py
@@ -1,8 +1,3 @@
-# in MySQLWorkbench I had to type this:
-    # UPDATE final_project.profile
-    # SET contactType = 1
-    # WHERE custContID = 1;
-
 import mysql.connector
 import pprint
 
@@ -10,46 +5,11 @@ import pprint
 connection = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "Nosplits35!",
+    password = "",
     database = "final_project"
 )
 
 cursor = connection.cursor()
-
-
-def convert(value: str):
-    '''
-    converts the given string passed as a parameter to be the most restrictive data type for the execute() function
-    so that when the cursor executes a query, it will try to force the types to match
-    '''
-    
-    types = [int, float, str]   # order needs to be this way bc int is most restrictive data type conversion
-    if value == '':
-        return None
-    for t in types:
-        try:
-            return t(value)
-        except:
-            pass
-
-
-def dataCleaner(path: str):
-    '''
-    reads in a CSV files & type converts to a list of tuples
-    returns a variable called dataCleaned of a list type
-    '''
-
-    with open(path, 'r', encoding = 'utf-8') as f:
-        data = f.readlines()
-
-    data = [line.strip().split(',') for line in data]
-    dataCleaned = []
-
-    for row in data[:]:
-        row = [convert(element) for element in row]
-        dataCleaned.append(tuple(row))
-    
-    return dataCleaned
 
 
 def modifyContactType(userInput: int, userID: int):  # fulfills UPDATE requirement of project
@@ -58,15 +18,9 @@ def modifyContactType(userInput: int, userID: int):  # fulfills UPDATE requireme
     this function is to update a preference about the way customer is notified about subscription
     '''
 
-    query = ''
+    query = "UPDATE profile SET contactType = %s WHERE custContID = %s;"
 
-    if userInput == 1:
-        query = "UPDATE profile SET contactType = " + str(userInput) + " WHERE custContID = " + str(userID) + ";"
-    
-    elif userInput == 0:
-        query = "UPDATE profile SET contactType = " + str(userInput) + " WHERE custContID = " + str(userID) + ";"
-
-    cursor.execute(query)
+    cursor.execute(query, (userInput, userID))
     connection.commit()
 
     print(f'{cursor.rowcount} record(s) updated')
@@ -87,7 +41,6 @@ def deleteAccount(givenUsername: str, recStatus: int = 1): # CHANGE ME: when thi
     '''
 
     cursor.execute(query, (recStatus, givenUsername))
-    # cursor.execute(query, (givenUsername, ))
     connection.commit()
 
 # gets input
